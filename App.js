e655f8131a296a370a418403f8d34aad27a688ba
Mher1997 <garanyan_97@mail.ru>
2019-09-07 00:27:48 +0400

    Add slider

--- App.js
+++ App.js
@@ -1,84 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Dimensions, Button } from 'react-native';
-import Carousel, {Pagination} from 'react-native-snap-carousel';
+import { YellowBox, View, StyleSheet } from 'react-native';
+import Slider from './components/Slider';
+YellowBox.ignoreWarnings(['Remote debugger']);
 
-let {height, width} = Dimensions.get('window');
-
-export default class extends React.Component{
-  
-
-  constructor(props){
-    super(props);
-    this.state = {
-      carouselItems : [
-        {
-          path : 'asset:/1.jpg',
-          text : '1'
-        },
-        {
-          path : 'asset:/2.jpg',
-          text : '1'
-        }
-      ],
-      activeIndex : 0
-    }
-  }
-
-  get pagination () {
-    const { carouselItems, activeIndex } = this.state;
-    return (
-        <Pagination
-          dotsLength={carouselItems.length}
-          activeDotIndex={activeIndex}
-          containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}
-          dotStyle={{
-              width: 10,
-              height: 10,
-              borderRadius: 5,
-              marginHorizontal: 8,
-              backgroundColor: 'rgba(255, 255, 255, 0.92)'
-          }}
-          inactiveDotStyle={{
-              // Define styles for inactive dots here
-          }}
-          inactiveDotOpacity={0.4}
-          inactiveDotScale={0.6}
-        />
-    );
-  }
-
-  _renderItem = ({item, index}) => (
-    <View style={{width, height: 200, flex: 1, justifyContent: 'center'}}>
-      <Image source={{uri : 'asset:/images/1.jpg'}} style={{width, height: 200,}}/>
-    </View>
-  )
-
-  onPress = () => this._carousel.snapToNext();
-
+class App extends React.Component {
   render(){
-    return (
-        <View style={styles.contanier}>
-          <Button title="CLick" onPress={()=>this.onPress()}/>
-          <Carousel
-            ref = { ref => this._carousel = ref }
-            data = {this.state.carouselItems}
-            renderItem = {this._renderItem}
-            sliderWidth = {width}
-            itemWidth = {width}
-            itemHeight = {1}
-            onSnapToItem = {index => this.setState({ activeIndex: index }) }
-          />
-          { this.pagination }
+    return(
+      <View>
+        <Slider/>
+        <View style={{width: '100%', position: 'absolute', bottom: 7, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <View style={styles.blackLine}/>
         </View>
-    );
+      </View>
+    )
   }
 }
 
-
 const styles = StyleSheet.create({
-  contanier: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+  blackLine: {
+    height: 5, 
+    width: 135,
+    backgroundColor: 'black', 
+    borderRadius: 3,
+  }
+})
+
+export default App;
